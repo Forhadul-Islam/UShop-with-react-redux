f61728abbf8055d4ddb780b24299246ff8f52d42
Forhadul Islam <forhadulislam2100@gmial.com>
2021-01-19 18:29:13 +0600

    working on menu

--- src/components/Home/menu/Menu.js
+++ src/components/Home/menu/Menu.js
@@ -4,7 +4,10 @@ const Menu = () => {
     return (
         <div className="menu">
             <div className="menu__title">
-                
+                title
+            </div>
+            <div className="menu__content">
+                content
             </div>
         </div>
     )
